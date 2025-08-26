8c58c7435defd92e22c2014f87e3e22a6a9186d2
sebastian <manijustworkhere@gmail.com>
2025-08-26 02:30:24 -0400

    i was tired of the plain ass white on white site so i made it right and... made it a cork/bulletin board

--- user.js
+++ user.js
@@ -1,28 +1,20 @@
 const postListEl = document.querySelector('.post-list')
+const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
-    const id = event.target.value
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-    postListEl.innerHTML = postsData.map(post => `
-        <div class="post">
-            <div class="post__title">
-                ${post.title}
-            </div>
-            <p class="post__body">
-                ${post.body}
-            </p>
-        </div>
-    `).join('')
+    const id = event.target.value;
+    renderPosts(id)
 }
 
-async function main() {
-    const id = localStorage.getItem("id")
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     //fetches an api dynamically
     const postsData = await posts.json();
+ postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
+}
 
- postListEl.innerHTML = postsData.map(post => `
+function postHTML(post) {
+return `
     <div class="post">
         <div class="post__title">
             ${post.title}
@@ -31,7 +23,7 @@ async function main() {
             ${post.body}
         </p>
     </div>
- `).join('')
+ `
 }
 
-main()
+renderPosts(id)
